a5dc8071c5a777a079d10ce60c11db835949c373
the14march <niekai1995@live.com>
2017-10-16 23:18:30 +0800

    添加使用包和模块组织结构，使用工厂模式组织程序

---
+++ app/main/views.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from flask import render_template, session, redirect, url_for, flash
+from app.main import main
+from app.main.forms import NameForm
+from app import db
+from app.models import User
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if app.config['IDACHENGZI_ADMIN']:
+                send_email(app.config['IDACHENGZI_ADMIN'], 'New User', 'mail/new_user', user=user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('main.index'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False), current_time = datetime.utcnow())
---
+++ config.py
@@ -0,0 +1,47 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or '2976626104@qq.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or '2407room-'
+
+    IDACHENGZI_MAIL_SUBJECT_PREFIX = '[Idachengzi]'
+    IDACHENGZI_MAIL_SENDER = 'Idachengzi Admin <2976626104@qq.com>'
+
+    IDACHENGZI_ADMIN = os.environ.get('IDACHENGZI_ADMIN') or '2976626104@qq.com'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('IDACHENGZI_DATABASE_URL') or 'mysql://root:5547@127.0.0.1/blog_idachengzi'
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('IDACHENGZI_DATABASE_URL') or 'mysql://root:5547@127.0.0.1/blog_idachengzi'
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('IDACHENGZI_DATABASE_URL') or 'mysql://root:5547@127.0.0.1/blog_idachengzi'
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
