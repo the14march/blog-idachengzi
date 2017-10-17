501f4c09f3b2b49c16a527cfc54aed400a4350ab
the14march <niekai1995@live.com>
2017-10-17 23:30:01 +0800

    添加了用户角色

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
 
+from app.models import Permission
+
 main = Blueprint('main', __name__)
 
 from app.main import views, errors
+
+
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
